ed5e024ec2dcc4035ccd70a4e89fc921c79bd965
talhacelikozgen <ozgentalha118@gmail.com>
2026-04-25 19:36:30 +0300

    asd

--- Workshop/2d_envanter_sistemi.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class 2denvantersistemi : MonoBehaviour
-{
-    void Start()
-    {
-        Debug.Log("🐉 Dragon: 2d envanter sistemi sistemi aktif.");
-    }
-}
--- Workshop/dusman_yapay_zekasi.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DüşmanYapayZekası : MonoBehaviour
-{
-    void Start()
-    {
-        Debug.Log("🐉 Dragon: Düşman Yapay Zekası aktif edildi.");
-    }
-}
